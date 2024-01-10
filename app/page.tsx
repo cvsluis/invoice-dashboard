eb3ec822830372f7647b2f24b4c6bcccc7e5989e
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-01-09 17:16:59 -0800

    refactor: app dashboard credits

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,14 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is an implementation by{' '}
+              <a
+                href="https://github.com/cvsluis/invoice-dashboard"
+                className="text-blue-500"
+              >
+                @cvsluis
+              </a>{' '}
+            of the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
